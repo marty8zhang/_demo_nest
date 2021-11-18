97b4e503077720190755ac00f2a44e463d62c50b
Marty Zhang <marty8zhang@gmail.com>
2021-11-18 22:49:41 +1100

    Introduce interface.
    Also update controller test case with 2 different mocking methods.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: 'AppServiceInterface',
+      useClass: AppService,
+    },
+  ],
 })
 export class AppModule {}
